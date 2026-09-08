b8f51a1460de195a94bced747c5682e481a562aa
atorneroc <107205481+atorneroc@users.noreply.github.com>
2026-09-08 15:11:51 -0500

    Add DEMO_TRAINING_SECRET5 for new MongoDB connection

--- src/demo_config.py
+++ src/demo_config.py
@@ -8,3 +8,4 @@ DEMO_TRAINING_SECRET = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@mergington-high
 DEMO_TRAINING_SECRET2 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@mergingtddsdon-high.avocado.mongodb.net"
 DEMO_TRAINING_SECRET3 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@mergingtddsdsson-high.avocado.mongodb.net"
 DEMO_TRAINING_SECRET4 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@mergingtddsdsssson-high.avocado.mongodb.net"
+DEMO_TRAINING_SECRET5 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@merssgingtddsdsssson-high.avocado.mongodb.net"
