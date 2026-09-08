a0359c79459a11fd125b5cddb9fd6cb2a30df0b3
atorneroc <107205481+atorneroc@users.noreply.github.com>
2026-09-08 15:14:45 -0500

    Add DEMO_TRAINING_SECRET7 to demo_config.py

--- src/demo_config.py
+++ src/demo_config.py
@@ -10,3 +10,4 @@ DEMO_TRAINING_SECRET3 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@mergingtddsdss
 DEMO_TRAINING_SECRET4 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@mergingtddsdsssson-high.avocado.mongodb.net"
 DEMO_TRAINING_SECRET5 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@merssgingtddsdsssson-high.avocado.mongodb.net"
 DEMO_TRAINING_SECRET6 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@merssgingtdds333dsssson-high.avocado.mongodb.net"
+DEMO_TRAINING_SECRET7 = "mongodb+srv://svc-admin:kLeioeBne5lsopPf@merssgin334gtdds333dsssson-high.avocado.mongodb.net"
